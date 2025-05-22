06dc0439861373f4504f0317a37db9087596a153
FrontEnd-Sungyu <jang.sungyu@cashwalk.io>
2025-05-22 12:07:58 +0900

    🛠️ fix : next config env 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-};
+  env: {
+    GCP_CLIENT_EMAIL: process.env.GCP_CLIENT_EMAIL,
+    GCP_PRIVATE_KEY: process.env.GCP_PRIVATE_KEY,
+  },
+}
 
-export default nextConfig;
+export default nextConfig
